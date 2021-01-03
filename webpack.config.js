dc2a12bd866c70bb92de402e9556992ac486904d
Kyle <kyleferry@icloud.com>
2021-01-03 04:00:38 +0000

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path');
- 
-module.exports = () => {
-    return {
-        entry:'./src/app.js',
-        output: {
-            path:path.join(__dirname,'public'),
-            filename: 'bundle.js'
-        },
-        mode:'development',
-        module: {
-            rules:[{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/,
-            }, {
-                test:/\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }]
-        },
-        devtool: 'eval-cheap-module-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            compress: true,
-            port: 9000
-          } 
-    };
-}
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
+const config = {
+    entry:'./src/app.js',
+    output: {
+        path:path.join(__dirname,'public'),
+        filename: 'bundle.[hash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({template: './index.html'})],
+    mode:'development',
+    module: {
+        rules:[{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }, {
+            test:/\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
+        compress: true,
+        port: 9000
+      } 
+};
+
+if(currentTask == "build"){
+    config.mode = "production"
+    config.devtool = "source-map"
+    config.module.rules[1].use[0] = MiniCssExtractPlugin.loader
+    config.plugins.push(new MiniCssExtractPlugin({filename: 'main.[hash].css'}), new CleanWebpackPlugin())
+}
+ 
+module.exports = config
 
 
